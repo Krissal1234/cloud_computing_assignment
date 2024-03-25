89728983b1b3e4df19bb6369832a6f6ccff778e7
= <salibakris03@gmail.com>
2024-03-25 18:09:28 +0100

    more setup

---
+++ backend/set_env_variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export CLOUD_SQL_PASSWORD='VZkQCTiCgcQFEolRCnHux8i1'
+export DB_INSTANCE_NAME='db-instance'
+export DB_NAME='number-generator'
+export DB_USER='number-generator'
+export PROJECT_NAME='cis3111-number-generator'
+export REGION='europe-west1'
