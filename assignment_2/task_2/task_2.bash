0535494c5c318512b90ee84ef1497be346d75265
krissal1234 <salibakris03@gmail.com>
2024-05-14 12:51:02 +0200

    final

--- assignment_2/task_2/task_2.bash
+++ assignment_2/task_2/task_2.bash
@@ -3,12 +3,12 @@
 minikube delete
 minikube start
 
+kubectl apply -f postgresql_setup/persistentVolume.yaml
+kubectl apply -f postgresql_setup/persistentVolumeClaim.yaml
 helm install postgresdb --namespace default bitnami/postgresql -f postgresql_setup/values.yaml
 
 kubectl apply -f postgresql_setup/secret.yaml
 kubectl apply -f postgresql_setup/configmap.yaml
-kubectl apply -f postgresql_setup/persistentVolume.yaml
-kubectl apply -f postgresql_setup/persistentVolumeClaim.yaml
 
 kubectl apply -f university_app/deployment.yaml
 kubectl apply -f university_grades/deployment.yaml
