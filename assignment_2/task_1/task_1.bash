295ad9d3202d02afe746b1b2dd9cd9a930cf5e95
krissal1234 <salibakris03@gmail.com>
2024-05-29 15:12:53 +0200

    final

--- assignment_2/task_1/task_1.bash
+++ assignment_2/task_1/task_1.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-#maybe think about making it handle if contaiers already are runninf
 export PGHOST="postgresDb"
 export PGPORT="5432"
 export PGDB="universitydb"
@@ -8,16 +7,24 @@ export PGPASS="pass"
 
 docker network create uni-net
 
-docker run -dit --name postgresDb --network uni-net \
-  -e POSTGRES_PASSWORD=$PGPASS \
-  -e POSTGRES_USER=$PGUSER \
-  -e POSTGRES_DB=$PGDB  \
-  postgres:latest 
+if [ "$(docker ps -q -f name=postgresDb)" ]; then
+    echo "PostgreSQL container is already running."
+else
+    docker run -dit --name postgresDb --network uni-net \
+      -e POSTGRES_PASSWORD=$PGPASS \
+      -e POSTGRES_USER=$PGUSER \
+      -e POSTGRES_DB=$PGDB  \
+      postgres:latest
+fi
 
-docker run -d --name enrollmentsApp --network uni-net -p 8080:8080 \
-  -e PGHOST=$PGHOST \
-  -e PGPORT=$PGPORT \
-  -e PGDB=$PGDB \
-  -e PGUSER=$PGUSER \
-  -e PGPASS=$PGPASS \
-  markvellaum/university:v0.0.1
+if [ "$(docker ps -q -f name=enrollmentsApp)" ]; then
+    echo "EnrollmentsApp container is already running."
+else
+    docker run -d --name enrollmentsApp --network uni-net -p 8080:8080 \
+      -e PGHOST=$PGHOST \
+      -e PGPORT=$PGPORT \
+      -e PGDB=$PGDB \
+      -e PGUSER=$PGUSER \
+      -e PGPASS=$PGPASS \
+      markvellaum/university:v0.0.1
+fi
